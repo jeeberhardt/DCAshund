9be9b4f6db9dd1d233ee99aaa303c6772053d9cb
Jerome Eberhardt <qksoneo@gmail.com>
2023-08-20 17:08:24 +0200

    Added function to get data from boursorama.

--- DCAshund/__init__.py
+++ DCAshund/__init__.py
@@ -5,6 +5,6 @@
 #
 
 from .dcashund import DCAshund
-from .utils import merge_stock_data, plot_simulation
+from .utils import merge_stock_data, plot_simulation, get_market_data_from_boursorama
 
-__all__ = ["DCAshund", "merge_stock_data", "plot_simulation"]
+__all__ = ["DCAshund", "merge_stock_data", "plot_simulation", "get_market_data_from_boursorama"]
--- DCAshund/utils.py
+++ DCAshund/utils.py
@@ -4,11 +4,101 @@
 # DCAshund - utils
 #
 
+import re
+import requests
 from pathlib import Path
 
 import pandas as pd
 import matplotlib.dates as mdates
+from bs4 import BeautifulSoup
 from matplotlib import pyplot as plt
+
+
+def get_market_data_from_boursorama(symbol, duration='4Y', start_date=None):
+    """
+    Get market data from boursorama.com
+    
+    Parameters
+    ----------
+    symbol : str
+        Symbol of the stock (e.g. 'AAPL' for Apple).
+    start_date : str
+        Start date of the data (default is None). Format is 'DD/MM/YYYY'.
+    duration : str
+        Duration of the data (default is '4Y').
+
+    Returns
+    -------
+    df : pandas.DataFrame
+        Dataframe with the data. Columns are ['Open', 'High', 'Low', 'Close']. Index is the date.
+    
+    """
+    base_url = "https://www.boursorama.com/_formulaire-periode/"
+    current_page = 1
+    number_pages = 1
+    duration_left = 0
+    data = []
+    if start_date is None:
+        start_date = ''
+
+    assert len(duration) >= 2, "Duration must be 2 characters. The first one is a number and the second one is the time unit (Y or M)."
+    assert duration[-1] in ['Y', 'M'], "The second character of duration must be Y or M."
+
+    if duration[-1] == 'Y':
+        if int(duration[:-1]) > 4:
+            duration_left = int(duration[:-1]) - 4
+            duration = '4Y'
+
+    while current_page <= number_pages:
+        tmp = []
+        parameters_url = f"page-{current_page}?symbol={symbol}&historic_search[duration]={duration}&historic_search[startDate]={start_date}"
+
+        response = requests.get(f"{base_url}{parameters_url}")
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        # Get number of pages
+        try:
+            table = soup.find_all("span", {"class": "c-pagination__content"})[-1]
+            number_pages = int(table.text)
+        except:
+            number_pages = 1
+
+        # Get data
+        regex = re.compile('c-table__cell *')
+        table = soup.find_all("td", {"class" : regex})
+
+        for i, t in enumerate(table):
+            if i % 6 == 0:
+                if tmp:
+                    data.append(tmp)
+                tmp = [t.text.strip()]
+            else:
+                tmp.append(t.text.strip())
+
+        data.append(tmp)
+        current_page += 1
+
+    try:
+        df = pd.DataFrame(data, columns=['Date', 'Close', 'Variation', 'High', 'Low', 'Open'])
+    except:
+        # No data, return empty dataframe
+        df = pd.DataFrame([], columns=['Open', 'High', 'Low', 'Close'])
+        return df
+
+    df['Date'] = pd.to_datetime(df['Date'], dayfirst=True)
+    df = df.set_index('Date')
+    df = df[['Open', 'High', 'Low', 'Close']]
+
+    if duration_left:
+        # Little trick to get data from the previous years > 4 years
+        start_date = df.index[-1] - pd.DateOffset(years=duration_left)
+        df2 = get_data_from_boursorama(symbol, f"{duration_left}Y", start_date.strftime("%d/%m/%Y"))
+        df = pd.concat([df2, df])
+        df = df[~df.index.duplicated(keep='first')]
+
+    df.sort_index(inplace=True)
+
+    return df
 
 
 def merge_stock_data(file_paths, fill_missing=True):
